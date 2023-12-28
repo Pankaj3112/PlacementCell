a25338ffa1c37d8c63f3f800fe1a653556a881b6
Pankaj <pankajbeniwal3112@gmail.com>
2023-12-28 17:00:25 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // method to connect to db
 async function main(){
     try{
-        const db = await mongoose.connect('mongodb+srv://pankaj3112:ftHLwuuPqJvyE9Qj@cluster0.lm6iyo2.mongodb.net/?retryWrites=true&w=majority');
+        const db = await mongoose.connect(process.env.MONGODB_URI);
         module.exports = db;
         console.log('**** MongoDB Connected ****')
     }
